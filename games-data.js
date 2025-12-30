eb8cd189b33d1ef325886b88386b37352fd88329
huynhphuc13012k5-coder <huynhphuc13012k5@gmail.com>
2025-12-30 21:59:15 +0700

    Add files via upload

---
+++ games-data.js
@@ -0,0 +1,50 @@
+const gamesData = [
+    {
+        id: 1,
+        name: "Pokemon Fire Red Apk",
+        image: "images/1.jpg", // Đường dẫn ảnh trong thư mục images
+		ss1: "all/3.gif", // <--- Thêm dòng này
+        ss2: "all/4.png", // <--- Thêm dòng này
+        description: "Bản Game GBA Pokemon Fire Red Huyền Thoại 1 thời\n 📦 Dung Lượng(Size) ~21MB",
+        downloadLink: "https://www.mediafire.com/file/v1zml0vkvn5vn0v/Pokemon_Fire_Red.apk/file" 
+    },
+    // Muốn thêm game chỉ cần copy khối {} ở trên và dán xuống đây
+	{
+        id: 2,
+        name: "Pokemon Emerald Apk",
+        image: "images/2.avif",
+		ss1: "all/1.jpg", // <--- Thêm dòng này
+        ss2: "all/2.png",
+	    description: "Bản Game GBA Pokemon Emerald Huyền Thoại 1 thời\n 📦 Dung Lượng(Size) ~20MB",
+        downloadLink: "https://www.mediafire.com/file/6fj2zetzwa8yzb4/Pokemon_Emerald.apk/file" 
+    },
+	{
+        id: 3,
+        name: "Pokémon Emerald Seaglass APK",
+        image: "images/3.jpg",
+		ss1: "all/5.webp", // <--- Thêm dòng này
+        ss2: "all/6.jpg",
+	    description: `Game Pokemon Emerald Seaglass
+		🎮Không giống như phiên bản Pokémon Emerald gốc, Emerald Seaglass có số lượng Pokémon được mở rộng đáng kể. 
+		1. Tất cả Pokémon từ Thế hệ 1 đến 3 đều có mặt, cùng với nhiều Pokémon tiến hóa giữa các thế hệ đến tận Thế 
+		hệ 9,bao gồm cả những lựa chọn phổ biến như Weavile, Tinkaton và Annihilape. Những Pokémon này được phân 
+		bổ cẩn thận trong trò chơi để tránh tình trạng quá tải và đảm bảo sự cân bằng trong quá trình chơi.
+		- Ngoài ra, nhiều Pokémon đã trải qua những thay đổi về hệ hoặc được điều chỉnh chỉ số để trở nên hấp dẫn và cân bằng hơn. Ví dụ:
+		Butterfree giờ có hệ Côn trùng/Tâm linh.
+        Blastoise thuộc hệ Nước/Thép.
+        Sunflora đã trở thành Grass/Fire.
+		2. Cơ chế chiến đấu được cải tiến:
+		- Cơ chế chiến đấu đã được cập nhật đáng kể để mang lại trải nghiệm hiện đại hơn:
+        - Phân loại chiêu thức vật lý/đặc biệt : Giờ đây, các chiêu thức được phân 
+        loại theo tính chất vật lý hoặc đặc biệt của chúng, thay vì theo loại.
+        Loại Tiên được thêm vào : Loại Tiên đã được tích hợp, giúp cân bằng bảng 
+		loại hiện có và bổ sung thêm các khả năng chiến thuật mới.
+        - Bộ chiêu thức được sửa đổi : Pokémon sử dụng bộ chiêu thức 
+		được sửa đổi từ Ultra Sun và Ultra Moon (USUM), làm cho các trận chiến 
+		trở nên năng động và mang tính chiến lược hơn.
+        - Chia sẻ EXP cho toàn nhóm : Người chơi được chia sẻ EXP cho toàn nhóm ngay từ đầu, với 
+		hệ thống giới hạn cấp độ linh hoạt để duy trì sự cân bằng và giảm thiểu việc cày cấp.
+		📦 Dung Lượng(Size) ~21MB"`,
+        downloadLink: "https://www.mediafire.com/file/jchxddlgf1jx9i8/Pokemon_Emerald_Seaglass_v3.0.apk/file" 
+    },
+];
